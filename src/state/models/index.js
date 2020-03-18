5acd6be17562aa0c402422970cb38093f36856d1
fdnklg <fabian.dinklage@me.com>
2020-03-18 10:14:18 +0100

    filter logic updated, state destructured

--- src/state/models/index.js
+++ src/state/models/index.js
@@ -1,9 +1,15 @@
 import MapModel from "./MapModel";
 import DataModel from "./DataModel";
 import TooltipModel from "./TooltipModel";
+import TagsModel from "./TagsModel";
+import FilterModel from "./FilterModel";
+import LegendModel from "./LegendModel";
 
 export default {
   ...MapModel,
   ...DataModel,
+  ...TagsModel,
+  ...FilterModel,
+  ...LegendModel,
   ...TooltipModel,
 };
